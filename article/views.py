d73b7ff6e720f6ad44233669d187b661f8b562e9
zuxriddin99 <56336321+zuxriddin99@users.noreply.github.com>
2020-10-26 12:18:11 +0500

    slug blog

--- article/views.py
+++ article/views.py
@@ -12,7 +12,7 @@ class PostListView(ListView):
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['asd'] = 'Category.objects.all()'
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['asd'] = 'Category.objects.all()'
+    #     return context
